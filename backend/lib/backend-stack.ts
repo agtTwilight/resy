76ce19d0d2973865c38abb8e19e68208b5bc4207
agtTwilight <lukasmacmillen@gmail.com>
2023-10-05 11:58:45 -0700

    pseudocode for graphql schema & init appsync x dynamodb setup

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,16 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import * as ddb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import * as path from "path";
 
 export class BackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
+		// Create the AppSync API
+		const api = new appsync.GraphqlApi(this, "Api", {
+			name: "reservations_api",
+			definition: appsync.Definition.fromFile(path.join(__dirname, "schema.graphql")),
+			authorizationConfig: {
+				defaultAuthorization: {
+					authorizationType: appsync.AuthorizationType.IAM,
+				},
+			},
+			xrayEnabled: true,
+		});
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		// Create DynamoDB table & table connection
+		const dynamodbTable = new ddb.Table(this, "DynamoDBTable", {
+			partitionKey: { name: "PK", type: ddb.AttributeType.STRING },
+			sortKey: { name: "SK", type: ddb.AttributeType.STRING },
+		});
+
+		const tableDatasource = api.addDynamoDbDataSource("DynamoDBTable", dynamodbTable);
+
+		// Create GraphQL resolvers for DynamoDB queries, mutations, etc.
+	}
 }
